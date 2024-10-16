873030dfc3d8f9ebb08c109fcb0118f75f99516e
Josh Bhattarai <josh.bhattarai@lilly.com>
2024-10-16 16:53:23 -0400

    Add some form validation to AddDevice and Edit Device forms

--- Experimental-Devices/management/forms.py
+++ Experimental-Devices/management/forms.py
@@ -14,7 +14,42 @@ class AddDeviceForm(forms.Form):
         subclasses = [model for model in apps.get_models() if issubclass(model, Device) and model is not Device]
         self.fields['device_type'].choices = [(subclass.__name__, subclass.__name__) for subclass in subclasses]
 
+    def clean(self):
+        cleaned_data = super().clean()
+        is_reserved = cleaned_data.get('is_reserved')
+        reserved_to = cleaned_data.get('reserved_to')
+
+        # Logic to set fields to None if conditions are met
+        if is_reserved is None:
+            cleaned_data['reserved_to'] = None
+            cleaned_data['reserved_at'] = None
+        elif reserved_to is None:
+            cleaned_data['is_reserved'] = None
+            cleaned_data['reserved_at'] = None
+
+        return cleaned_data
+
+
 class DeviceForm(forms.ModelForm):
     class Meta:
         model = Device
-        fields = ['serial_number', 'description', 'location', 'is_reserved', 'reserved_to']  # Include fields you want to edit
+        fields = ['serial_number', 'description', 'location', 'is_reserved',
+                  'reserved_to']  # Include fields you want to edit
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        is_reserved = cleaned_data.get('is_reserved')
+        reserved_to = cleaned_data.get('reserved_to')
+
+        # If is_reserved is False, set reserved_to and reserved_at to None
+        if is_reserved is False:
+            cleaned_data['reserved_to'] = None
+            cleaned_data['reserved_at'] = None
+
+        # If reserved_to is None, set reserved_at and is_reserved to None
+        if reserved_to is None:
+            cleaned_data['reserved_at'] = None
+            cleaned_data['is_reserved'] = False  # Set to False if reserved_to is None
+
+        return cleaned_data
